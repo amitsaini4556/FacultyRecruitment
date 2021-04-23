580c7b5e90ac35f938a25829256a1e37a750f950
amitsaini4556 <amitsaini2701@gmail.com>
2021-04-23 19:42:38 +0530

    functional definition added of signIn or Dashboard method.

---
+++ facultyRecruitment/accounts/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .models import *
+# Create your views here.
+
+# User can signIn with the username and password
+def signIn(request):
+
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password =request.POST.get('password')
+	
+		user = authenticate(request, username=username, password=password)
+
+		if user is not None:
+			login(request, user)
+			return redirect('dashboard')
+		else:
+			messages.info(request, 'Username OR password is incorrect')
+
+	context = {}
+	return render(request, 'accounts/signIn.html', context)
+
+
+def dashboard(request):
+    return render(request,'accounts/dashboard.html')
