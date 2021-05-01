4f259adcf514eab10dd2a2af9064cf1b675c29a5
amitsaini4556 <amitsaini2701@gmail.com>
2021-05-01 19:05:59 +0530

    url added for signUP as register

--- facultyRecruitment/userAccounts/urls.py
+++ facultyRecruitment/userAccounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
 	path('login/', views.signIn, name="login"),
+	path('register/', views.signUp, name="register"),
 	path('', views.dashboard, name="dashboard"),
 ]
