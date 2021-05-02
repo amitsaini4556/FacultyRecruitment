15aeb89ae5e8416aebbd4b47b5ecb2e8f2a387c5
amitsaini4556 <amitsaini2701@gmail.com>
2021-05-02 12:32:26 +0530

    email validation added.

--- facultyRecruitment/userAccounts/forms.py
+++ facultyRecruitment/userAccounts/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from validate_email import validate_email
 
 from .models import *
 
@@ -9,3 +10,14 @@ class CreateUserForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
+
+	def clean_email(self):
+		email = self.cleaned_data.get('email')
+		if validate_email(email,verify=True) == True:
+			try:
+				match = User.objects.get(email = email)
+			except User.DoesNotExist:
+				return email
+			raise forms.ValidationError('This email address is already in use.')
+		else:
+			raise forms.ValidationError('This email address is Invalid.')
