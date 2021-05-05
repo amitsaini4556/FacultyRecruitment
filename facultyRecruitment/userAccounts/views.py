c03636f9e8293b1207dbfca02c730d143e9616d7
Amit saini <54165387+amitsaini4556@users.noreply.github.com>
2021-05-05 17:14:01 +0530

    SignUP UI added with functionality (#7)
    * DataBase
    
    * user creation form created
    
    * clickable functionality added for signUp button.
    
    * user signUP UI designed.
    
    * URL added for signUP as register
    
    * Signup Functionality.
    
    * email validation added.

--- facultyRecruitment/userAccounts/views.py
+++ facultyRecruitment/userAccounts/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 # Create your views here.
 
 # User can signIn with the username and password
@@ -23,6 +25,23 @@ def signIn(request):
 
 	context = {}
 	return render(request, 'userAccounts/signIn.html', context)
+
+def signUp(request):
+
+	form = CreateUserForm()
+	if request.method == 'POST':
+		form = CreateUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = form.cleaned_data.get('username')
+
+
+			messages.success(request, 'Account was created for ' + username)
+
+			return redirect('login')
+
+	context = {'form':form}
+	return render(request, 'userAccounts/signUp.html', context)
 
 
 def dashboard(request):
