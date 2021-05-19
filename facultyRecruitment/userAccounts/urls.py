f5704e567bd9328ef905f54dca0b3448f63741be
amitsaini4556 <amitsaini2701@gmail.com>
2021-05-19 10:52:11 +0530

    - url added for signOut method

--- facultyRecruitment/userAccounts/urls.py
+++ facultyRecruitment/userAccounts/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
 	path('login/', views.signIn, name="login"),
+	path('logout/', views.signOut, name="logout"),
 	path('register/', views.signUp, name="register"),
 	path('', views.dashboard, name="dashboard"),
 ]
