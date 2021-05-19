519b34b479c435ae5325d4b1fa0543232f62104f
amitsaini4556 <amitsaini2701@gmail.com>
2021-05-19 11:06:15 +0530

    - AddJob Method added.

--- facultyRecruitment/jobs/views.py
+++ facultyRecruitment/jobs/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from userAccounts.decorators import allowed_users, admin_only
 
+@admin_only
+def addJob(request):
+    return render(request,'jobs/addJobForm.html')
 
-def showAddJobForm(request):
+@login_required(login_url='login')
+def applyJob(request):
     return render(request,'jobs/addJobForm.html')
