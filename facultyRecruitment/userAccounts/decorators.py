c20dd6566db7a69d578fb70be17c2ff1b98c91e0
amitsaini4556 <amitsaini2701@gmail.com>
2021-05-19 12:33:40 +0530

    - dashboard changed by jobs

--- facultyRecruitment/userAccounts/decorators.py
+++ facultyRecruitment/userAccounts/decorators.py
@@ -32,7 +32,7 @@ def admin_only(view_func):
 			group = request.user.groups.all()[0].name
 
 		if group == 'candidates':
-			return redirect('dashboard')
+			return redirect('jobs')
 
 		if group == 'admin':
 			return view_func(request, *args, **kwargs)
