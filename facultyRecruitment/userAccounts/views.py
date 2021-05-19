ef024dd8e7ca20b75be9b90958748e84dbe42a5c
amitsaini4556 <amitsaini2701@gmail.com>
2021-05-19 10:50:54 +0530

    - Applicants added default in candidates group. - SignOut method added.

--- facultyRecruitment/userAccounts/views.py
+++ facultyRecruitment/userAccounts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import *
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
+from django.contrib.auth.models import Group
 # Create your views here.
 
 # User can signIn with the username and password
@@ -26,6 +27,10 @@ def signIn(request):
 	context = {}
 	return render(request, 'userAccounts/signIn.html', context)
 
+def signOut(request):
+	logout(request)
+	return redirect('dashboard')
+
 def signUp(request):
 
 	form = CreateUserForm()
@@ -34,7 +39,8 @@ def signUp(request):
 		if form.is_valid():
 			user = form.save()
 			username = form.cleaned_data.get('username')
-
+			group = Group.objects.get(name = 'candidates')
+			user.groups.add(group)
 
 			messages.success(request, 'Account was created for ' + username)
 
